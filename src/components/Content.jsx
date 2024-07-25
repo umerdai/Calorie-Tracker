3237cd2f5510e15e2c93e3be7703433804c9c397
Omer <omerabdullah317@gmail.com>
2024-07-25 13:52:36 +0500

    debugging

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -11,23 +11,28 @@ function Content() {
 
   return (
     <div>
-    <div className="container">
-      <video autoPlay loop muted playsInline className="bg-video">
-        <source src={bgVideo} type="video/mp4" />
-      </video>
-      <div data-aos="fade-up" className="text-center text-white p-6  rounded-lg ">
+      <div className="container">
+        <video autoPlay loop muted playsInline className="bg-video">
+          <source src={bgVideo} type="video/mp4" />
+        </video>
+        <div
+          data-aos="fade-up"
+          className="text-center text-white p-6  rounded-lg "
+        >
           <h1 className="text-4xl mb-4">Join Us on Our Fitness Journey!</h1>
           <p className="text-xl mb-4">
-            Transform your life with our personalized plans, expert guidance, and supportive community.
+            Transform your life with our personalized plans, expert guidance,
+            and supportive community.
           </p>
           <p className="text-xl mb-4">
-            Start today and achieve your weight loss and exercise goals with Track it.
+            Start today and achieve your weight loss and exercise goals with
+            Track it.
           </p>
           <p className="text-xl">
             Together, we can make a healthier, happier you!
           </p>
         </div>
-    </div>
+      </div>
     </div>
   );
 }
